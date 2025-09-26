90fc17228710c21dd905e57cdb2dcc606aa3c9b5
Chris Wolfe <me@crlfe.ca>
2025-09-26 11:58:02 -0400

    Add less confusing home page and examples directory

--- plugins/transform-html.ts
+++ plugins/transform-html.ts
@@ -1,24 +1,27 @@
 import { Plugin } from "vite";
 
-const REMOVE_PATTERNS = [
+const REPLACE_PATTERNS = [
+  // Compact all whitespace.
+  [/\s+/g, " "],
+
   // Remove crossorigin from script tags (these are added by Vite).
-  /(?<=<script[^>]*\s)crossorigin\s*/g,
+  [/(?<=<script[^>]*\s)crossorigin\s*/g, ""],
 
   // Remove trailing whitespace and slash in a self-closing tag.
-  /\s*\/(?=>)/g,
+  [/\s*\/(?=>)/g, ""],
 
   // Remove whitespace after an HTML tag.
-  /(?<=>)\s+/g,
+  [/(?<=>)\s+/g, ""],
 
   // Remove whitespace before an HTML tag.
-  /\s+(?=<)/g,
-];
+  [/\s+(?=<)/g, ""],
+] satisfies [RegExp, string][];
 
 export default (): Plugin => {
   return {
     name: "transform-html",
     transformIndexHtml(html) {
-      return REMOVE_PATTERNS.reduce((s, p) => s.replaceAll(p, ""), html);
+      return REPLACE_PATTERNS.reduce((s, [p, r]) => s.replaceAll(p, r), html);
     },
   };
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,53 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import reportBundleSize from "./plugins/report-bundle-size";
 import shortCssNames from "./plugins/short-css-names";
 import transformHtml from "./plugins/transform-html";
 import transformShaders from "./plugins/transform-shaders";
+import { opendir } from "node:fs/promises";
 
-export default defineConfig({
-  plugins: [
-    reportBundleSize(),
-    shortCssNames(),
-    transformHtml(),
-    transformShaders(),
-  ],
-  base: "",
-  build: {
-    modulePreload: false,
-    rollupOptions: {
-      output: {
-        assetFileNames: "a/[hash].[ext]",
-        chunkFileNames: "a/[hash].js",
-        entryFileNames: "a/[hash].js",
+const root = dirname(fileURLToPath(import.meta.url));
+
+async function getExamples(): Promise<string[]> {
+  const result: string[] = [];
+
+  const dir = await opendir(resolve(root, "examples"), { recursive: true });
+  for await (const entry of dir) {
+    if (entry.isFile() && entry.name.endsWith(".html")) {
+      console.log(entry.parentPath, entry.name);
+      result.push(resolve(entry.parentPath, entry.name));
+    }
+  }
+
+  result.sort();
+  return result;
+}
+
+export default async () =>
+  defineConfig({
+    plugins: [
+      reportBundleSize(),
+      shortCssNames(),
+      transformHtml(),
+      transformShaders(),
+    ],
+    appType: "mpa",
+    base: "",
+    resolve: {
+      alias: {
+        myjam: resolve(root, "src"),
       },
     },
-  },
-});
+    build: {
+      modulePreload: false,
+      rollupOptions: {
+        input: [resolve(root, "index.html"), ...(await getExamples())],
+        output: {
+          assetFileNames: "a/[hash].[ext]",
+          chunkFileNames: "a/[hash].js",
+          entryFileNames: "a/[hash].js",
+        },
+      },
+    },
+  });
