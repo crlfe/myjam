f421028004a33223ffa0ea5545120348c53af0e4
Chris Wolfe <me@crlfe.ca>
2025-11-01 21:17:45 -0400

    Add ESLint checks for CSS and HTML files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,28 @@
+import css from "@eslint/css";
 import eslint from "@eslint/js";
+import html from "@html-eslint/eslint-plugin";
 import jsdoc from "eslint-plugin-jsdoc";
 import prettier from "eslint-plugin-prettier/recommended";
 import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
+/** @type {Parameters<defineConfig>[0]} */
+const scriptCommon = {
+  rules: {
+    "capitalized-comments": ["error"],
+    eqeqeq: ["error"],
+    "no-plusplus": ["error"],
+    "no-use-before-define": ["error"],
+    "no-ternary": ["error"],
+    "prefer-template": ["error"],
+  },
+};
+
 export default defineConfig(
   {
     files: ["**/*.{c,m,}js{x,}"],
     extends: [
+      scriptCommon,
       jsdoc.configs["flat/recommended-typescript-flavor"],
       eslint.configs.recommended,
     ],
@@ -15,22 +30,42 @@ export default defineConfig(
   {
     files: ["**/*.{c,m,}ts{x,}"],
     extends: [
+      scriptCommon,
       jsdoc.configs["flat/recommended-typescript"],
       eslint.configs.recommended,
       tseslint.configs.strict,
       tseslint.configs.stylistic,
     ],
   },
-  prettier,
-  globalIgnores(["**/dist"]),
   {
+    files: ["**/*.css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
     rules: {
-      "capitalized-comments": "error",
-      eqeqeq: "error",
-      "no-plusplus": "error",
-      "no-use-before-define": "error",
-      "no-ternary": "error",
-      "prefer-template": "error",
+      "css/use-baseline": ["error", { available: "newly" }],
     },
   },
+  {
+    files: ["**/*.html"],
+    language: "html/html",
+    plugins: { html },
+    extends: ["html/recommended"],
+    rules: {
+      // Tweak format checks to match prettier.
+      "html/indent": ["error", 2, {}],
+      "html/no-extra-spacing-attrs": [
+        "error",
+        { enforceBeforeSelfClose: true },
+      ],
+      "html/require-closing-tags": [
+        "error",
+        {
+          selfClosing: "always",
+        },
+      ],
+    },
+  },
+  prettier,
+  globalIgnores(["**/dist"]),
 );
